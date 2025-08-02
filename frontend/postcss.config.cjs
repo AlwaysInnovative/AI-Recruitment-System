0657776b7e27a534ad4963f18d5e4623991b2ef4
AlwaysInnovative <alwaysinnovative5608@gmail.com>
2025-08-02 16:35:54 +0100

    Update postcss.config.cjs

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,8 +1,6 @@
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {
-      tailwindcss: { config: './tailwind.config.cjs' }
-    },
-    autoprefixer: {}
-  }
-}
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
