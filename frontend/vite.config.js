2e88ca5b378ae55cef2c23f3735c09e2baaf42dc
AlwaysInnovative <alwaysinnovative5608@gmail.com>
2025-08-03 18:42:17 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
+  css: {
+    postcss: './postcss.config.cjs'
+  }
 })
