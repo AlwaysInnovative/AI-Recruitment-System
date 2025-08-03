8005cf10befb0a91aaabdc8470873dcbb20d19bb
AlwaysInnovative <alwaysinnovative5608@gmail.com>
2025-08-03 20:44:33 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,43 +1,43 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    eslint()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@utils': path.resolve(__dirname, './src/utils')
-    }
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://ai-recruitment-system.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false
-      }
+      'lodash': path.resolve(__dirname, 'node_modules/lodash'),
     }
   },
   build: {
-    outDir: 'dist',
-    sourcemap: true,
+    commonjsOptions: {
+      include: [/lodash/, /node_modules/]
+    },
     rollupOptions: {
+      external: ['react', 'react-dom'],
       output: {
         manualChunks: {
-          react: ['react', 'react-dom', 'react-router-dom'],
-          ui: ['@radix-ui/react-dialog', '@radix-ui/react-dropdown-menu'],
-          vendor: ['date-fns', 'zod', 'lodash']
+          lodash: ['lodash'],
+          radix: [/@radix-ui/],
+          vendor: ['react', 'react-dom', 'react-router-dom']
         }
       }
     }
+  },
+  optimizeDeps: {
+    include: ['lodash']
   }
 });
